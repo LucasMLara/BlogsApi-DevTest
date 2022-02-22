07d066d2110d37010de8fa2cd83e46ae33cb8f5c
Lucas Lara <lucasml@live.com>
2022-02-22 16:56:48 -0300

    feat: ✨ Adiciona Middleware para Login

---
+++ src/middlewares/login.js
@@ -0,0 +1,14 @@
+const { StatusCodes: { BAD_REQUEST } } = require('http-status-codes');
+const { User } = require('../models');
+const { loginSchema } = require('../validations/schema');
+
+const login = async (req, _res, next) => {
+  const { email, password } = req.body;
+  const { error } = loginSchema.validate({ email, password });
+  if (error) return next({ statusCode: BAD_REQUEST, message: error.message });
+  const checkIfUserExists = await User.findOne({ where: { email } });
+  if (!checkIfUserExists) return next({ statusCode: BAD_REQUEST, message: 'Campos inválidos' });
+  return next();
+};
+
+module.exports = login;
