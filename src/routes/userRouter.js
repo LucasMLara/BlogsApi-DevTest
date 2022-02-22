020bdd3223dfc60db55476b18f4fdaba3b909eb3
Lucas Lara <lucasml@live.com>
2022-02-22 20:23:45 -0300

    feat: ✨ getUserById Rota

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { createNewUser, getEveryone } = require('../controllers');
-const { validUser, Auth } = require('../middlewares');
+const { createNewUser, getEveryone, getById } = require('../controllers');
+const { validUser, Auth, checkIfUserExists } = require('../middlewares');
 
 const router = Router();
 
 router.post('/', validUser, createNewUser);
 router.get('/', Auth, getEveryone);
+router.get('/:id', Auth, checkIfUserExists, getById);
 
 module.exports = router;
