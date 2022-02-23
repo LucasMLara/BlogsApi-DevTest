93f14e16893bdf0bf3091a174b78abded02d6dfc
Lucas Lara <lucasml@live.com>
2022-02-23 17:38:09 -0300

    feat: ✨ Router getAllposts

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { createNewPost } = require('../controllers');
+const { createNewPost, getPosts } = require('../controllers');
 
 const { validPost, Auth } = require('../middlewares');
 
 const router = Router();
 
 router.post('/', Auth, validPost, createNewPost);
+router.get('/', Auth, getPosts);
 
 module.exports = router;
