93d8ce4bc4c847731ea9c00c60562d7f72716534
Lucas Lara <lucasml@live.com>
2022-02-23 18:40:10 -0300

    feat: ✨ getPostById - CheckIfPostExist Middleware

--- src/middlewares/post.js
+++ src/middlewares/post.js
@@ -1,5 +1,6 @@
-const { StatusCodes: { BAD_REQUEST } } = require('http-status-codes');
+const { StatusCodes: { BAD_REQUEST, NOT_FOUND } } = require('http-status-codes');
 const { postSchema } = require('../validations/schema');
+const { Post } = require('../models');
 
 const validPost = async (req, _res, next) => {
   const { title, content } = req.body;
@@ -8,6 +9,16 @@ const validPost = async (req, _res, next) => {
   return next();
 };
 
+const checkIfPostExists = async (req, _res, next) => {
+  const { id } = req.params;
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return next({ statusCode: NOT_FOUND, message: 'Post não existe' });
+  }
+  return next();
+};
+
 module.exports = {
   validPost,
+  checkIfPostExists,
 };
