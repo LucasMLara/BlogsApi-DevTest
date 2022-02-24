63dcbafc9833dcdb9e32fa308ec32b3d163111fd
Lucas Lara <lucasml@live.com>
2022-02-24 00:02:21 -0300

    feat: 🔒️ Adiciona variáveis de ambiente em parâmetros do crypto

--- src/seeders/20220222143611-users.js
+++ src/seeders/20220222143611-users.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+const { SALT, ITERATIONS, KEYLEN, DIGEST, SEED_PASSWORD } = process.env
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('Users',
@@ -5,21 +7,21 @@ module.exports = {
         id: 1,
         displayName: 'Silvinha Gianattasio',
         email: 'silvinha.gianattasio@trybe.com',
-        password: '123456',
+        password: crypto.pbkdf2Sync(SEED_PASSWORD, SALT, +ITERATIONS, +KEYLEN, DIGEST).toString('utf-8'),
         image: '../../assets/silvinha.png',
       },
       {
         id: 2,
         displayName: 'Marina Drummond',
         email: 'marina.drummond@trybe.com',
-        password: '123456',
+        password: crypto.pbkdf2Sync(SEED_PASSWORD, SALT, +ITERATIONS, +KEYLEN, DIGEST).toString('utf-8'),
         image: '../../assets/mari.jpeg',
       },
       {
         id: 3,
         displayName: 'Matheus Goyas',
         email: 'matheus.goyas@trybe.com',
-        password: '123456',
+        password: crypto.pbkdf2Sync(SEED_PASSWORD, SALT, +ITERATIONS, +KEYLEN, DIGEST).toString('utf-8'),
         image: '../../assets/goyas.jpeg',
       },
       ], { timestamps: false });
