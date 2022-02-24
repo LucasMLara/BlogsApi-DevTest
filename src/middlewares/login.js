21ed07423d9dd1ab0f62aff70e4825790f07edd8
Lucas Lara <lucasml@live.com>
2022-02-24 00:12:37 -0300

    feat: 🔒️ Altera parâmetros para utilização de variáveis de ambiente - LoginMiddleware

--- src/middlewares/login.js
+++ src/middlewares/login.js
@@ -1,13 +1,19 @@
+const crypto = require('crypto');
 const {
   StatusCodes: { BAD_REQUEST },
 } = require('http-status-codes');
 const { User } = require('../models');
 const { loginSchema } = require('../validations/schema');
 
+const {
+  SALT, ITERATIONS, KEYLEN, DIGEST,
+} = process.env;
+
 const login = async (req, _res, next) => {
-  const { email, password } = req.body;
-  const { error } = loginSchema.validate({ email, password });
+  const { email, password: vulnerablePassword } = req.body;
+  const { error } = loginSchema.validate({ email, password: vulnerablePassword });
   if (error) return next({ statusCode: BAD_REQUEST, message: error.message });
+  const password = crypto.pbkdf2Sync(vulnerablePassword, SALT, +ITERATIONS, +KEYLEN, DIGEST).toString('utf-8');
   const checkIfUserExists = await User.findOne({ where: { email, password } });
   if (!checkIfUserExists) { return next({ statusCode: BAD_REQUEST, message: 'Campos inválidos' }); }
   return next();
