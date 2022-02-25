dbabab9a6a6e0b0bc343a86f91a76ea39c6b62a4
Lucas Lara <lucasml@live.com>
2022-02-25 14:48:44 -0300

    test: 🚧 chaiHttp Requests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,15 +1,28 @@
 const chai = require("chai");
 const { expect } = chai;
+const sinon = require('sinon')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+// https://app.betrybe.com/course/live-lectures/sd-cohort-10-a#aula-283-nodejs-testando-apis-com-testes-de-integracao
+
+const server = require('../index')
 
 const {
   StatusCodes: { CREATED, BAD_REQUEST, OK, UNAUTHORIZED, CONFLICT, NOT_FOUND, NO_CONTENT },
 } = require("http-status-codes");
 
 describe("1 - Utilizando a rota '/users' ", () => {
-  describe("ao tentar criar um novo usuário", () => {
+  describe("ao tentar criar um novo usuário com sucesso", async () => {
     let response;
+  
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva@trybe.com', password: '123456',
+    })
 
-    it('retorna Status HTTP 201 - "CREATED"', () => {
+    it.only('retorna Status HTTP 201 - "CREATED"', () => {
       expect(response).to.have.status(CREATED);
     });
 
@@ -20,9 +33,26 @@ describe("1 - Utilizando a rota '/users' ", () => {
     it('com a propriedade "token"', () => {
       expect(response.body).to.have.property("token");
     });
+    it("e que o token não venha vazio", () => {
+      expect(response.body.token).to.not.be.empty;
+    })
   });
 
   describe("Checa se o usuário é único", () => {
+    let response;
+    before(async()=> {
+      await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva@trybe.com', password: '123456',
+    })
+    })
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva@trybe.com', password: '123456',
+    })
+
     it("retorna status HTTP - 409 'CONFLICT' ", () => {
       expect(response).to.have.status(CONFLICT);
     });
@@ -38,13 +68,28 @@ describe("1 - Utilizando a rota '/users' ", () => {
   });
 
   describe("checa se o usuário está inserindo os inputs corretamente", () => {
-    it("Verifica se 'Nome' foi inserido!", () => {
+    
+    it("Verifica se 'displayName' foi inserido!", () => {
+      let response;
+      response = await chai.request(server)
+      .post('/users')
+      .send({
+      email: 'gabrielOliva@trybe.com', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("'displayName' is required");
     });
-    it("Verifica se 'Nome' possui ao menos 8 caracteres!", () => {
+
+    it("Verifica se 'displayName' possui ao menos 8 caracteres!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel', email: 'gabrielOliva@trybe.com', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -53,13 +98,26 @@ describe("1 - Utilizando a rota '/users' ", () => {
       );
     });
     it("Email é requerido!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("'email' is required");
     });
     it("Email sem o nome do usuário!", () => {
-      // sem o nome do email
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: '@trybe.com', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -68,7 +126,14 @@ describe("1 - Utilizando a rota '/users' ", () => {
       );
     });
     it("Email sem o domínio!", () => {
-      // sem o dominio do email
+
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -77,12 +142,26 @@ describe("1 - Utilizando a rota '/users' ", () => {
       );
     });
     it("Password é requerido!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva@trybe.com',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("'password' is required");
     });
-    it("Verifica se 'Email' possui ao menos 6 caracteres!", () => {
+    it("Verifica se 'password' possui ao menos 6 caracteres!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/users')
+    .send({
+      displayName: 'Gabriel Oliva', email: 'gabrielOliva@trybe.com', password: '12345',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -95,6 +174,13 @@ describe("1 - Utilizando a rota '/users' ", () => {
 
 describe("2 - Utilizando a rota '/login'", () => {
   describe("chega se o usuário loga da forma correta", () => {
+    let response;
+
+    response = await chai.request(server)
+    .post('/login')
+    .send({
+      email: 'gabrielOliva@trybe.com', password: '123456',
+    })
     it('retorna Status HTTP 200 - "OK"', () => {
       expect(response).to.have.status(OK);
     });
@@ -105,14 +191,29 @@ describe("2 - Utilizando a rota '/login'", () => {
       expect(response.body).to.have.property("token");
     });
   });
+
   describe("checa se o usuário está inserindo os inputs corretamente", () => {
     it("Email é requerido!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/login')
+    .send({
+      password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("'email' is required");
     });
     it("Email não pode ser vazio!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/login')
+    .send({
+      email: '', password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -121,12 +222,26 @@ describe("2 - Utilizando a rota '/login'", () => {
       );
     });
     it("Password é requerido!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/login')
+    .send({
+      password: '123456',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("'password' is required");
     });
     it("Password não pode ser vazio!", () => {
+      let response;
+
+    response = await chai.request(server)
+    .post('/login')
+    .send({
+      email: 'gabrielOliva@trybe.com', password: '',
+    })
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -135,6 +250,28 @@ describe("2 - Utilizando a rota '/login'", () => {
       );
     });
     it("Verifica se as credenciais estão incorretas", () => {
+      let response;
+
+      response = await chai.request(server)
+      .post('/login')
+      .send({
+        email: 'gabrielOliva@trybe.com', password: '654321',
+      })
+      
+      expect(response).to.have.status(BAD_REQUEST);
+      expect(response.body).to.be.a("object");
+      expect(response.body).to.have.property("message");
+      expect(response.body.message).to.be.equal("Campos inválidos");
+    });
+    it("Verifica se as credenciais estão incorretas", () => {
+      let response;
+
+      response = await chai.request(server)
+      .post('/login')
+      .send({
+        email: 'gabrielOlivaDaSilva@trybe.com', password: '123456',
+      })
+      
       expect(response).to.have.status(BAD_REQUEST);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -143,19 +280,44 @@ describe("2 - Utilizando a rota '/login'", () => {
   });
 });
 
-describe("3 - Utilizando a rota GET '/users'", () => {
+describe("3 - Utilizando a rota GET '/user'", () => {
   describe("Verifica-se a possibilidade de obter a lista de todos os usuários", () => {
+    let response;
+    let Authorization;
+
+    before(async() => {
+      const {
+        body: { token },
+      } = await chai.request(server).post("/login").send({
+        email: "silvinha.gianattasio@trybe.com",
+        password: "123456",
+      });
+      Authorization = token;
+    })
+    
     it("Devidamente autenticado", () => {
+      response = await chai
+        .request(server)
+        .get("/user")
+        .set({Authorization})
       expect(response).to.have.status(OK);
       expect(response.body).to.be.a("array");
     });
     it("Sem o token", () => {
+      response = await chai
+        .request(server)
+        .get("/user")
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("Token não encontrado");
     });
     it("Token adulterado", () => {
+      const wrongToken = "notRealToken"
+      response = await chai
+        .request(server)
+        .get("/user")
+        .set({Authorization: wrongToken})
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -164,9 +326,25 @@ describe("3 - Utilizando a rota GET '/users'", () => {
   });
 });
 
-describe("4 - Utilizando a rota GET '/users/:id'", () => {
+describe("4 - Utilizando a rota GET '/user/:id'", () => {
+  let response;
+    let Authorization;
+
+    before(async() => {
+      const {
+        body: { token },
+      } = await chai.request(server).get("/login").send({
+        email: "marina.drummond@trybe.com",
+        password: "123456",
+      });
+      Authorization = token;
+    })
   describe("Verifica-se retorna a pessoa usuária buscada", () => {
     it("Devidamente autenticado", () => {
+      response = await chai
+        .request(server)
+        .get("/user/1")
+        .set({Authorization})
       expect(response).to.have.status(OK);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("id");
@@ -175,18 +353,30 @@ describe("4 - Utilizando a rota GET '/users/:id'", () => {
       expect(response.body).to.have.property("image");
     });
     it("se a pessoa usuária não existe", () => {
+      response = await chai
+        .request(server)
+        .get("/user/99999999999999999999999999")
+        .set({Authorization})
       expect(response).to.have.status(NOT_FOUND);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("Usuário não existe");
     });
     it("Sem o token", () => {
+      response = await chai
+        .request(server)
+        .get("/user/1")
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("Token não encontrado");
     });
     it("Token adulterado", () => {
+      const wrongToken = 'xablau'
+      response = await chai
+        .request(server)
+        .get("/user/1")
+        .set({Authorization: wrongToken})
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
@@ -197,16 +387,37 @@ describe("4 - Utilizando a rota GET '/users/:id'", () => {
 
 describe("5 - Utilizando a rota DELETE '/users/me'", () => {
   describe("Verifica-se deleta o usuário", () => {
+    before(async() => {
+      const {
+        body: { token },
+      } = await chai.request(server).get("/login").send({
+        email: "marina.drummond@trybe.com",
+        password: "123456",
+      });
+      Authorization = token;
+    })
     it("Obtendo sucesso", () => {
+      response = await chai
+        .request(server)
+        .delete("/user/me")
+        .set({Authorization})
       expect(response).to.have.status(NO_CONTENT);
     });
     it("Sem o token", () => {
+      response = await chai
+        .request(server)
+        .delete("/user/me")
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
       expect(response.body.message).to.be.equal("Token não encontrado");
     });
     it("Token adulterado", () => {
+      const wrongToken = 'SempreÉBomPraticar'
+      response = await chai
+        .request(server)
+        .delete("/user/me")
+        .set({Authorization: wrongToken})
       expect(response).to.have.status(UNAUTHORIZED);
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
